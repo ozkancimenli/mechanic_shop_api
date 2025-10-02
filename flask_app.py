fae2865ff426865de45a5d4b25f32b7f4b6e7979
Ozkan Cimenli <ozkan.cimenli@hotmail.com>
2025-10-02 14:33:53 -0400

    Prepare for deployment

--- flask_app.py
+++ flask_app.py
@@ -1,15 +1,15 @@
 # flask_app.py
-from app import create_app
 import os
+from app import create_app
+from instance.config import DevelopmentConfig, ProductionConfig
 
 def get_config():
-
     if os.environ.get("PRODUCTION"):
-        return "ProductionConfig"
-    return "config.py"
+        return ProductionConfig
+    return DevelopmentConfig
 
+# Flask app instance
 app = create_app(get_config())
 
 if __name__ == "__main__":
-
     app.run(debug=True)
--- instance/config.py
+++ instance/config.py
@@ -1,18 +1,24 @@
-#instance/config.py
+# instance/config.py
 import os
 
 class DevelopmentConfig:
     SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:Bunuhat1rla@localhost/mechanic_shop_db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
     CACHE_TYPE = "SimpleCache"
+    SECRET_KEY = "dev-secret-key"  
+
 
 class TestingConfig:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///:testing.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
     CACHE_TYPE = "SimpleCache"
-
+    SECRET_KEY = "test-secret-key"
 
 
 class ProductionConfig:
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     CACHE_TYPE = "SimpleCache"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret")
